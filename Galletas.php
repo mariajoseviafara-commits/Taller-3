ca1f3bb2dbc6c5f08fe3be898224f33b222248f6
mariajoseviafara-commits <maria.jose.viafara@correounivalle.edu.co>
2025-09-24 12:08:34 -0500

    Update Galletas.php
    Cambios en el código

--- Galletas.php
+++ Galletas.php
@@ -12,5 +12,67 @@
 <body>
 <h1 class="txt">Hecho por María José Viafara (2438446)</h1>
  
+
+    <h2 class= "nombre">RECETA DE GALLETAS</h2>
+    <img class="header" src="Img/header.jpg" />
+
+
+<br>
+<br>
+<br>
+<h1 class="tit"> Integrientes:  </h1>
+<br>
+<br>
+<h1 class="txt"> 250 gramos Harina De Trigo </h1>
+<br>
+<h1 class="txt">50 gramos Polvo de Cacao </h1>
+<br>
+<h1 class="txt">125 gramos Mantequilla Sin Sal a temperatura ambiente </h1>
+<br>
+<h1 class="txt">1 Unidad Huevo</h1>
+<br>
+<h1 class="txt">1 Cucharadita Esencia De Vainilla </h1>
+<br>
+<h1 class="txt">1 Pizca Sal</h1>
+<br>
+<h1 class="txt">1 Tableta Chocolate Blanco</h1>
+<br>
+<br>
+<br>
+<h1 class="tit"> ¡A cocinar! </h1>
+<br>
+<h1 claas="tit"> Prepara los ingredientes </h1>
+<br>
+<h1 class="txt">1. Cierne harina, Cacao en Polvo y sal. Reserva. En otro recipiente bate la mantequilla con el azúcar impalpable hasta que la mezcla esté cremosa.</h1>
+<br>
+<h1 class="tit"> Prepara la masa </h1>
+<img src="Img/img1.jpg" />
+<br>
+<h1 class="txt">2. Mezcla la preparación de la mantequilla con el huevo y añade la esencia de vainilla. A velocidad baja incorpora la mezcla de ingredientes secos sobre la crema de mantequilla, poco a poco hasta formar una masa que se despegue de los lados del recipiente donde estamos batiendo.</h1>
+<br>
+<h1 class="tit"> Estira la masa </h1>
+<img src="Img/img2.jpg" />
+<br>
+<h1 class="txt">3. Prepara 2 láminas de papel antiadherente, coloca la masa en medio del papel y estírala con ayuda de un bolillo hasta lograr un grosor de más o menos 0,5 cm. Una vez lista la masa, enróllala en el bolillo y refrigérala mínimo 1 hora.</h1>
+<br>
+<h1 class="tit"> Cocina las galletas</h1>
+<img src="Img/img3.jpg" />
+<br>
+<h1 class="txt">4. Precalienta el horno a 180° C. Una vez transcurrido el tiempo en refrigeración de la masa, retírala del frío y estírala sobre el mesón. Usando cortadores de galletas, dale forma a la masa y coloca las galletas en una lata cubierta con papel antiadherente. Lleva al horno durante 8 a 10 minutos hasta que los bordes se doren ligeramente.</h1>
+<br>
+<h1 class="tit"> Decora </h1>
+<img src="Img/img4.jpg" />
+<br>
+<h1 class="txt">5. Una vez listas las galletas, retira del horno y deja enfriar completamente. Mientras se enfrían, derrite el Chocolate Blanco a baño maría. Una vez derretido completamente decora las galletas con líneas de chocolate blanco en la superficie. Disfruta.</h1>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+
+
+
+<Label> 
 </body>
 </html>
